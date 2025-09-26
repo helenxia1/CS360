831ca02fdf407f6d7b97ece05d34cf4af53f1f10
Helen Xia <helenxia@usc.edu>
2025-09-27 02:34:11 +0900

    submission lab 1

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -50,6 +50,35 @@ def breadth_first_search(stack):
 
     # --- v ADD YOUR CODE HERE v --- #
 
+    if stack.check_ordered():
+        return flip_sequence
+    
+    visited = set()
+    curr_stack = stack.copy()
+    queue = deque([(curr_stack, flip_sequence)])
+    
+    while queue:
+        curr_stack, curr_flip = queue.popleft() #remove the first thing in queue everytime
+        state_tuple = (tuple(curr_stack.order),tuple(curr_stack.orientations))
+        if state_tuple in visited:
+            continue
+        visited.add(state_tuple)
+
+        n = curr_stack.num_books
+
+        for i in range (1, n+1):
+            new_stack = curr_stack.copy()
+            new_stack.flip_stack(i)
+            new_flip = curr_flip + [i]
+
+            if new_stack.check_ordered(): # if list is ordered and flipped to 1 now
+                return new_flip
+            
+            new_tuple = (tuple(new_stack.order),tuple(new_stack.orientations))
+
+            if new_tuple not in visited:
+                queue.append((new_stack, new_flip)) #not visited so add to queue
+
     return flip_sequence
     # ---------------------------- #
 
@@ -58,6 +87,30 @@ def depth_first_search(stack):
     flip_sequence = []
 
     # --- v ADD YOUR CODE HERE v --- #
+    if stack.check_ordered():
+        return flip_sequence
+    
+    visited = set()
+    curr_stack = stack.copy()
+    queue = [(stack.copy(), flip_sequence)]
+    
+    while queue: 
+        curr_stack, flip_sequence = queue.pop()
+
+        state_tuple = (tuple(curr_stack.order), tuple(curr_stack.orientations))
+        if state_tuple in visited:
+            continue
+        visited.add(state_tuple)
+
+        if curr_stack.check_ordered():
+            return flip_sequence
+
+        n = curr_stack.num_books
+        for i in range(1, n+1):
+            next = curr_stack.copy()
+            next.flip_stack(i)
+            next_sqc = flip_sequence + [i]
+            queue.append((next,next_sqc))
 
 
     return flip_sequence
